f783ad1a6589999ee061a6c27025b323a1b880a8
Ritik <ritik09052000@gmail.com>
2020-08-04 17:39:15 +0530

    made checkout and cart dynamic

--- store/models.py
+++ store/models.py
@@ -48,6 +48,19 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
+
 # Orderitem table
 
 
@@ -58,6 +71,12 @@ class OrderItem(models.Model):
         Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    # defining own property to find total
+    @property
+    def get_total(self):
+        total = self.product.price*self.quantity
+        return total
 
 # shipping model
 
